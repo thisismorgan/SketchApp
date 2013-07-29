499f939e02e7450e75d08a6609fceea43add9a75
Morgan <mkgriggs@gmail.com>
2013-07-29 15:49:45 -0700

    Generated a penTool function

--- js/penTool.js
+++ js/penTool.js
@@ -1,47 +1,113 @@
-
-
+// grabbing the canvas from the html page and assigning it to the JS variable canvas
 var canvas = document.getElementById("art");
-var context = canvas.getContext("2d");
-context.fillStyle = "rbg(255,0,0)";
 
+// creation of the penTool class
+function penTool(canvas, event, buffer){
+	this.increment = 2
+	this.buffer = buffer;
+	this.canvas = canvas;
+	this.context = this.canvas.getContext("2d");
+	this.event = event;
+	this.current_segment = [];
+	this.all_segments = [];
+	this.line_drawn = false;
+	
+	var getMousePos = function(canvas, event)
+	{
+		var rect = canvas.getBoundingClientRect();
+		return{
+			x: event.clientX - rect.left,
+			y: event.clientY - rect.top - 0.5625
 
-function getMousePos(canvas, evt) {
-	var rect = canvas.getBoundingClientRect();
-	return {
-		x: evt.clientX - rect.left,
-		y: evt.clientY - rect.top - 0.5625
-	};
-}
-
-function addPosToBuffer(x, y) {
-	buffer[x][y] = 1
-}
-
-
-// tracks movements and adds to the buffer
-canvas.addEventListener("mousedown", function(evt){
-	var mousePos = getMousePos(canvas, evt);
-	var x = mousePos.x;
-	var y = mousePos.y;
-	addPosToBuffer(x, y);
-}, false);
-
-canvas.addEventListener("mousemove", function(evt){
-	var mousePos = getMousePos(canvas, evt);
-	var x = mousePos.x;
-	var y = mousePos.y;
-	addPosToBuffer(x, y);
-}, false);
-
-// Draws what's on the buffer to the canvas
-canvas.addEventListener("mouseup", function(){
-	for (i=0; i<max_x; i++){
-		for (j=0; j<max_y; j++){
-			if (buffer[i][j] == 1){
-				context.fillStyle = "rgb(139,137,137)";
-				context.fillRect(i, j, 2, 2);
-			}
-		}
+		};
 	}
-}, false);
+	
+	var addPosToBuffer = function(x, y, increment)
+	{
+		buffer[x][y] = this.increment
+	}
 
+	var addStartPosToBuffer = function (x, y)
+	{
+		buffer[x][y] = 1
+
+	}
+
+	var draw = function(pair, current_segment, all_segments, context)
+	{
+		current_segment.push(pair);
+		for(i=0; i<current_segment.length; i++){
+			if (i == 0) {
+				var start_point = current_segment[0];
+				context.beginPath();
+				context.moveTo(start_point.x, start_point.y);
+			}
+			if (i%3 == 0) {
+				var next_point = current_segment[i];
+				context.lineTo(next_point.x, next_point.y);
+			}
+			context.lineWidth = 1;
+			context.lineCap = "round";
+			context.strokeStyle = "rgb(139, 137, 137)";
+			context.stroke();
+		}
+		
+		all_segments.push(current_segment);
+		current_segment = [];
+		return current_segment;
+	}
+
+	this.handleEvent = function(event){
+		switch(event.type){
+			case 'mousedown':
+				var mousePos = getMousePos(canvas, event);
+				var x = mousePos.x;
+				var y = mousePos.y;
+				var start = {
+					x: x,
+					y: y
+				};
+				addStartPosToBuffer(x, y);
+				this.current_segment.push(start)
+				this.line_drawn = true;
+				break;
+			case 'mousemove':
+			    if (this.line_drawn == true){
+				var mousePos = getMousePos(canvas, event);
+				var x = mousePos.x;
+				var y = mousePos.y;
+				var next = {
+					x: x,
+					y: y
+				};
+				addPosToBuffer(x, y, this.increment);
+				this.current_segment.push(next)
+				this.increment = this.increment + 1
+				}
+				break;
+			case 'mouseup':
+				var mousePos = getMousePos(canvas, event);
+				var x = mousePos.x;
+				var y = mousePos.y;
+				var last = {
+					x: x,
+					y: y
+				};
+				addPosToBuffer(x, y, this.increment);
+				draw(last, this.current_segment, this.all_segments, this.context);
+				this.current_segment = [];
+				this.line_drawn = false
+				break;
+		}
+	};
+
+
+	canvas.addEventListener('mousedown', this, false);
+	canvas.addEventListener('mousemove', this, false);
+	canvas.addEventListener('mouseup', this, false);
+
+
+}
+
+// an instance of the penTool class
+var brush = new penTool(canvas, event, buffer);
