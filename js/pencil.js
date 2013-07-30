6898002f4167b20d6b767bfbb395c906b256b36a
Morgan <mkgriggs@gmail.com>
2013-07-30 11:56:00 -0700

    Pixel & Pencil Tools Added

---
+++ js/pencil.js
@@ -0,0 +1,34 @@
+// an instance of the penTool class -- draws lines between points in the path
+function pencil(){
+	penTool.call(this);
+}
+
+pencil.prototype = new penTool();
+pencil.prototype.constructor = pencil;
+
+pencil.prototype.draw = function(pair, current_segment, all_segments, context)
+{
+	console.log("Line drawn function called.")
+	current_segment.push(pair);
+	for(i=0; i<current_segment.length; i++){
+		if (i == 0) {
+			var start_point = current_segment[0];
+			context.beginPath();
+			context.moveTo(start_point.x, start_point.y);
+		}
+		if (i%3 == 0) {
+			var next_point = current_segment[i];
+			context.lineTo(next_point.x, next_point.y);
+		}
+		context.lineWidth = 1;
+		context.lineCap = "round";
+		context.strokeStyle = "rgb(139, 137, 137)";
+		context.stroke();
+	}
+	
+	all_segments.push(current_segment);
+	current_segment = [];
+	return current_segment;
+}
+
+pencil1 = new pencil();
---
+++ js/pixel.js
@@ -0,0 +1,26 @@
+// an instance of the penTool class -- draws a path of squares
+function pixel(){
+	penTool.call(this);
+}
+
+pixel.prototype = new penTool();
+pixel.prototype.constructor = pixel;
+
+pixel.prototype.draw = function(pair, current_segment, all_segments, context)
+{
+	console.log("Pixel drawn function called.")
+	current_segment.push(pair);
+	for (i=0; i<max_x; i++){
+		for (j=0; j<max_y; j++){
+			if (buffer[i][j] != 0){
+				context.fillStyle = "rgb(139,137,137)";
+				context.fillRect(i, j, 2, 2);
+			}
+		}
+	}
+	all_segments.push(current_segment);
+	current_segment = [];
+	return current_segment;
+}
+
+pixel1 = new pixel();
